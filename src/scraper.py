7635a165c39945a139283ce65a9a3f93f8682d76
Victor <victormachado.cons@gmail.com>
2022-04-01 10:22:25 -0300

    Novos dados e melhorias no scraper

--- src/scraper.py
+++ src/scraper.py
@@ -34,13 +34,18 @@ def processGen(str):
     else:
         return -1
 
+def csvString(str):
+    return str
+    #return '\"'+str+'\"'
+
 class PokemonMovesScraper:
     url = 'https://bulbapedia.bulbagarden.net'
     magic_filter1 = '#mw-content-text > div > table:nth-child(4) > tbody > tr > td > table > tbody'
     magic_filter2 = '#mw-content-text > div  table[style*="float:right"] > tbody'
-    magic_filter3 = '#mw-content-text > div > table:nth-child(14) > tbody'
+    magic_filter3 = '#By_leveling_up'
     moves_url = []
     moves = []
+    moves_pkmn_list = []
     moves_count = 0
 
     def __init__(self):
@@ -61,15 +66,23 @@ class PokemonMovesScraper:
         info_box_addr = soup.select_one(self.magic_filter2)
         info_box_addr2 = info_box_addr.select_one('tr:nth-of-type(4)').findAll('tr')
 
-        move_name = info_box_addr.select_one('b').text
-        move_type = info_box_addr2[0].select_one('th + td span > b').text
-        move_category = info_box_addr2[1].select_one('th + td span').text
+        move_name = csvString(info_box_addr.select_one('b').text)
+        move_type = csvString(info_box_addr2[0].select_one('th + td span > b').text)
+        move_category = csvString(info_box_addr2[1].select_one('th + td span').text)
         move_PP = processNumber(info_box_addr2[2].select_one('td').text)
         move_power = processNumber(info_box_addr2[3].select_one('td').text)
         move_accuracy = processNumber(info_box_addr2[4].select_one('td').text)
         move_gen = processGen(info_box_addr.select_one('tr:nth-of-type(6) th + td a').text)
         move_id = self.moves_count + 1
 
+        move_pkmn_list = []
+
+        if soup.select_one(self.magic_filter3) != None:
+            pkmn_table = soup.select_one(self.magic_filter3).parent.findNext('table').findAll('tr')[2:-1]
+            for pkmn in pkmn_table:
+                move_pkmn_list.append(int(processNumber(pkmn.select_one('td').text)))
+
+        self.moves_pkmn_list.append([move_id,move_pkmn_list])
         self.moves.append([move_id, move_name, move_type, move_category, move_PP, move_power, move_accuracy, move_gen])
         self.moves_count += 1
     def processMovesURL(self):
@@ -78,13 +91,24 @@ class PokemonMovesScraper:
             self.processMoveURL(url)
     def dumpMoves(self):
         with open('moves.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
+            writer = csv.writer(csvfile, delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
             writer.writerow(['move_id','move_name','move_type','move_category','move_PP','move_power','move_accuracy','move_gen'])
             for move in self.moves:
                 writer.writerow(move)
+    def dumpMovesPKMN(self):
+        with open('movespkmn.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(['entry_id','move_id','pkmn_id',])
+            entry_id = 1
+            for move_pkmn_list in self.moves_pkmn_list:
+                for pkmn_id in move_pkmn_list[1]:
+                    writer.writerow([entry_id, move_pkmn_list[0], pkmn_id])
+                    entry_id += 1
 
-scraper = PokemonMovesScraper()
+#scraper = PokemonMovesScraper()
 
-scraper.extractMovesURL()
-scraper.processMovesURL()
-scraper.dumpMoves()
+#scraper.extractMovesURL()
+#scraper.processMovesURL()
+
+#scraper.dumpMoves()
+#scraper.dumpMovesPKMN()
