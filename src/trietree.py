f0b75c036b0654c2a21196aa154fe560a7474fca
Paulo-V-Luz <peres.paulo14@gmail.com>
2022-04-27 07:40:49 -0300

    arvore triee inicio

--- src/trietree.py
+++ src/trietree.py
@@ -1,6 +1,9 @@
 from blockmanager import *
 from filemanager import *
-
+#Adapted from: https://gist.github.com/joaofeitoza13/166758e842635b081db0922b13a8146f
+#Original code by joaofeitoza13
+#New changes:
+#
 class TNode:
     pass
 
